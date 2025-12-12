d69204c28a3d5729f0eec20644ed2817a2afea8a
harish gehlot <gehloth03@gmail.com>
2025-12-12 17:07:35 +0000

    adding just

---
+++ app/training_endpoint.py
@@ -0,0 +1,19 @@
+from ml_system.feature_pipeline import FeaturePipeline
+from ml_system.training_pipeline import TrainingPipeline
+from omegaconf import DictConfig
+import hydra
+
+@hydra.main(config_path='../configs', config_name='config', version_base=None)
+def main(cfg: DictConfig):
+    if cfg.pipeline.stage == 'feature':
+        FeaturePipeline(cfg).execute()
+    elif cfg.pipeline.stage == 'train':
+        TrainingPipeline(cfg).execute()
+    else:
+        FeaturePipeline(cfg).execute()
+        TrainingPipeline(cfg).execute()
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ justfile
@@ -0,0 +1,11 @@
+full:
+    @echo "Full Pipeline (Feature + Training) run started.."
+    @python app/training_endpoint.python
+
+feature:
+    @echo "Feature Pipeline Only started.."
+    @python app/training_endpoint.py pipeline.stage=feature
+
+train:
+    @echo "Training Pipeline Only started.."
+    @python app/training_endpoint.py pipeline.stage=train
