ceecaa89e887923145be271cf73bd208692db2dc
harish gehlot <gehloth03@gmail.com>
2025-12-12 19:13:31 +0000

    data loader

--- src/ml_system/feature_pipeline.py
+++ src/ml_system/feature_pipeline.py
@@ -1,6 +1,10 @@
+from ml_system.scripts.data.loader import fetch_raw_data
+
+
 class FeaturePipeline:
     def __init__(self, configs):
         self.configs = configs
+
     def execute(self):
+        fetch_raw_data(configs=self.configs)
         print("Feature Pipeline Done..")
-        
