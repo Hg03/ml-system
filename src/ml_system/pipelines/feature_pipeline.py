6168eba09a179bd3c4e3baac1675a91c7099f2e4
harish gehlot <gehloth03@gmail.com>
2025-12-13 11:08:15 +0530

    rollback

--- src/ml_system/pipelines/feature_pipeline.py
+++ src/ml_system/pipelines/feature_pipeline.py
@@ -1,9 +1,6 @@
-from ml_system.scripts.data.loader import fetch_raw_data
-
 class FeaturePipeline:
     def __init__(self, configs):
         self.configs = configs
     def execute(self):
-        fetch_raw_data()
         print("Feature Pipeline Done..")
         
