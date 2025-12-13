8822a70af945e98bde697a17112154659f4ade81
harish gehlot <gehloth03@gmail.com>
2025-12-13 08:13:52 +0000

    adding to handle failure cases

--- src/ml_system/scripts/utils/loading_utils.py
+++ src/ml_system/scripts/utils/loading_utils.py
@@ -3,8 +3,11 @@ from typing import Any
 import joblib
 
 def load_(path: str, format: str) -> Any:
-    if format == 'parquet':
-        return pl.read_parquet(path)
-    elif format == 'model':
-        return joblib.load(path)
+    try:
+        if format == 'parquet':
+            return pl.read_parquet(path)
+        elif format == 'model':
+            return joblib.load(path)
+    except Exception as e:
+        raise FileNotFoundError(f"Didn't able to find the location or file for {format} specified at {path}")
 
