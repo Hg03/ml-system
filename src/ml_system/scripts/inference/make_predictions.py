30311926153f95aabb28d431225d8a546b39c08c
harish gehlot <gehloth03@gmail.com>
2026-01-11 15:16:38 +0000

    single inference implemented

--- src/ml_system/scripts/inference/make_predictions.py
+++ src/ml_system/scripts/inference/make_predictions.py
@@ -1,9 +1,23 @@
 from ml_system.scripts.features.feature_store import get_registered_model
 from omegaconf import DictConfig
+import polars as pl
+import joblib
+import shutil
 from typing import Any
 
 def infer(configs: DictConfig, input: Any):
     model = get_registered_model(configs=configs)
+    shutil.rmtree(configs.models.paths.registry_model)
     model.download(configs.models.paths.registry_model)
     
-    # Will perform further inference process
+    # Load the model
+    loaded_model = joblib.load(configs.models.paths.registry_model + '/full_model_pipeline.joblib')
+    
+    # Convert input dictionary to DataFrame
+    # Wrap each value in a list to create a single-row DataFrame
+    input_data_dict = {key: [value] for key, value in input.items()}
+    input_data = pl.DataFrame(input_data_dict)
+    
+    # Make prediction
+    prediction = loaded_model.predict(input_data)
+    return f"{prediction[0]}"
